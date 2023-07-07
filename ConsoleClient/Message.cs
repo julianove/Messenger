48339799498ae3fa738c49feea1419f155a27c85
julianove <julian.and.data@gmail.com>
2023-07-07 20:41:59 +0300

    added a message class

---
+++ ConsoleClient/Message.cs
@@ -0,0 +1,24 @@
+﻿public class Message
+{
+    public string UserName { get; set; }
+    public string MessageText { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public Message(string userName, string messageText, DateTime timeStamp)
+    {
+        UserName = userName;
+        MessageText = messageText;
+        TimeStamp = timeStamp;
+    }
+    public Message()
+    {
+        UserName = "System";
+        MessageText = "Server is runnung...";
+        TimeStamp = DateTime.Now;
+    }
+
+    public override string? ToString()
+    {
+        string output = String.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+        return output;
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message();
+
+            Console.WriteLine(msg.ToString());
+        }
+    }
+}
